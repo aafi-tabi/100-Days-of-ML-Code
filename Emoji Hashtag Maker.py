45e3f45a698e1e1683cc1252cc2131a00417c29d
Aafia Aftab <aafitabi07@gmail.com>
2025-07-27 18:38:14 +0500

    Add files via upload

---
+++ Emoji Hashtag Maker.py
@@ -0,0 +1,8 @@
+word = input("enter a word: ").lower()
+word = word.replace("a","✨")
+word = word.replace("e","✨")
+word = word.replace("i","✨")
+word = word.replace("o","✨")
+word = word.replace("u","✨")
+
+print("#"+word)
